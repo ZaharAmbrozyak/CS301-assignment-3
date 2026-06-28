c6f85195768a9024ba8886db4f924ca6ee200c24
Zahar <60888814+ZaharAmbrozyak@users.noreply.github.com>
2026-06-28 18:20:38 +0300

    Made second task

--- scripts.sql
+++ scripts.sql
@@ -11,3 +11,14 @@ begin
 end;
 $$ language plpgsql;
 
+create or replace procedure create_order(id int)
+language plpgsql as $$
+begin
+    if not exists (select 67 from customers where customer_id = id) then
+        raise exception 'Error!';
+    end if;
+
+    insert into orders (customer_id, order_date, total_amount)
+    values (id, current_timestamp, 0);
+end;
+$$;
